94937941b05c12b6bd0043864367d773836e5a2c
Fenrir2539 <swansonalex@gmail.com>
2021-04-10 10:28:21 -0700

    ALEX-1 Initialize project

---
+++ src/main/java/com/fraggkyrie/chinookapi/ChinookapiApplication.java
@@ -0,0 +1,13 @@
+package com.fraggkyrie.chinookapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChinookapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChinookapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fraggkyrie/chinookapi/ChinookapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fraggkyrie.chinookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChinookapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
